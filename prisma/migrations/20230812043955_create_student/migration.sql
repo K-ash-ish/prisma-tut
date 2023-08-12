610f89b0041006af6bf56f8af6bf52cb616929a6
K-ash-ish <kashishsondhiya@gmail.com>
2023-08-12 17:52:21 +0530

    feat: created student table

---
+++ prisma/migrations/20230812043955_create_student/migration.sql
@@ -0,0 +1,14 @@
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('Male', 'Female');
+
+-- CreateTable
+CREATE TABLE "Student" (
+    "id" SERIAL NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "gender" "Gender" NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "Student_pkey" PRIMARY KEY ("id")
+);
