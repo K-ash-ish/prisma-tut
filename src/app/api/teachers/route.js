1fea56cf2e0686cf6955dfaea4c020c04f202dd1
K-ash-ish <kashishsondhiya@gmail.com>
2023-08-12 20:39:03 +0530

    feat: created teachers route to fetch details

---
+++ src/app/api/teachers/route.js
@@ -0,0 +1,31 @@
+import { prisma } from "@/db/prisma";
+import { NextResponse } from "next/server";
+import { z } from "zod";
+
+const teacherSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  age: z.number().min(24).max(50),
+  gender: z.enum(["Male", "Female"]),
+  email: z.string().email(),
+});
+
+export async function GET() {
+  const teachers = await prisma.teacher.findMany();
+  return NextResponse.json(teachers);
+}
+export async function POST(req, res) {
+  try {
+    const teacher = teacherSchema.parse(await req.json());
+    const teacherRes = await prisma.teacher.create({
+      data: teacher,
+    });
+    return NextResponse.json(teacherRes);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "Something Went wrong!" },
+      { status: 500 }
+    );
+  }
+}
