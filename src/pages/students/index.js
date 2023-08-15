13cc10429078a5b3520071cc68e7419e7711bbb1
K-ash-ish <kashishsondhiya@gmail.com>
2023-08-15 15:01:28 +0530

    feat: creating students page route

---
+++ src/pages/students/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+function page() {
+  return (
+    <>
+      <Head>
+        <title>Student Page</title>
+      </Head>
+      <div>StudentPage</div>
+    </>
+  );
+}
+
+export default page;
