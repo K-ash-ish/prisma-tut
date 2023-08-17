7d6cca36040d6fb38656246ae88b5a3e90c52c68
K-ash-ish <kashishsondhiya@gmail.com>
2023-08-17 22:01:55 +0530

    feat: created basic homepage ui using shadcnUI

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 function Layout({ children }) {
   return (
     <>
-      <nav>
-        <h1>School DB</h1>
+      <nav className=" text-center my-4">
+        <Link className="text-2xl font-bold  " href={"/"}>
+          School DB
+        </Link>
       </nav>
-      <main>{children}</main>
-      <footer>Footer</footer>
+      <main className="w-full flex justify-center">{children}</main>
     </>
   );
 }
